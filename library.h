753bb06ea74cd8be66d96eb04fc4c6e387bb3e52
arnav-singla-cmyk <axnav.singla.372@gmail.com>
2026-09-01 12:35:47 +0530

    Update library.h

--- library.h
+++ library.h
@@ -1,81 +1,6 @@
+#pragma once
+#include "utils.h"
 #include "issue.h"
-
-int num_f(const char *msg = "", int menu = 0)
-{
-    int x;
-    while (true)
-    {
-        try
-        {
-            cout << msg;
-            cin >> x;
-            if (cin.fail())
-            {
-                throw "ENTER VALID DATA-TYPE";
-            }
-            if (menu == 1 && (x > 4 || x < 1))
-            {
-                throw 1.5;
-            }
-            break;
-        }
-
-        catch (const char *e)
-        {
-            cout << "Error: " << e << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-        catch (double e)
-        {
-            cout << "ERROR : Enter an integer between 1 to 4 !!";
-        }
-    }
-    return x;
-}
-
-void delay_f(int ms)
-{
-    clock_t start = clock();
-
-    while ((clock() - start) * 1000 / CLOCKS_PER_SEC < ms)
-        ;
-}
-
-void loading_f()
-{
-    char spin[] = {'|', '/', '-', '\\'};
-
-    cout << "\nLoading ";
-
-    for (int i = 0; i < 20; i++)
-    {
-        cout << spin[i % 4];
-        cout.flush();
-        delay_f(100);
-        cout << '\b';
-    }
-}
-
-void clear_f()
-{
-#ifdef _WIN32
-    system("cls");
-#else
-    system("clear");
-#endif
-}
-
-void pause_f()
-{
-#ifdef _WIN32
-    system("pause");
-#else
-    cout << "\nPress Enter to continue...";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cin.get();
-#endif
-}
 
 void final_menu()
 {
@@ -88,40 +13,32 @@ void final_menu()
         cout << "\n2. MANAGE Books";
         cout << "\n3. MANAGE Issue System";
         cout << "\n4. Exit";
-        choice = num_f("\n\nEnter your choice: ", 1);
+        choice = readInt("\n\nEnter your choice: ", 1, 4);
 
         switch (choice)
         {
         case 1:
-
             student_menu();
-
             break;
-
         case 2:
-
             book_menu();
-
             break;
-
         case 3:
-
             issue_menu();
-
             break;
-
         case 4:
             cout << "Thank You";
             break;
-
         default:
             cout << "\nInvalid!";
         }
+
         if (choice != 4)
         {
-            pause_f();
-            loading_f();
-            clear_f();
+            pauseScreen();
+            loading();
+            clearScreen();
         }
+
     } while (choice != 4);
 }
